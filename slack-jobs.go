445eff9053c85f9a2a6128a0a104ea1c5d24af0d
Viet Pham <phamhongviet52@gmail.com>
2015-03-08 13:42:10 +0700

    add token filter

--- slack-jobs.go
+++ slack-jobs.go
@@ -20,6 +20,7 @@ var (
 	REDIS string
 	CLASS string
 	QUEUE string
+	TOKENS map[string]bool
 )
 
 func main() {
@@ -28,12 +29,17 @@ func main() {
 	p_redis := flag.String("r", "localhost:6379", "redis host")
 	p_class := flag.String("c", "SlackOPS", "resque class")
 	p_queue := flag.String("q", "slackops", "resque queue")
+	p_tokens := flag.String("t", "", "slack tokens, split by a comma (,)")
 	flag.Parse()
 
 	PORT = ":" + *p_port
 	REDIS = *p_redis
 	CLASS = *p_class
 	QUEUE = *p_queue
+	TOKENS = make(map[string]bool)
+	for _, t := range strings.Split(*p_tokens, ",") {
+		TOKENS[t] = true
+	}
 
 	// connect to redis and add queue
 	rcon, err := redis.Dial("tcp", REDIS)
@@ -58,51 +64,57 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 		Args map[string]string `json:"args"`
 	}
 
-	body := make([]byte, r.ContentLength)
-
 	// accept only POST
-	if r.Method == "POST" {
-		// make connection to redis
-		rcon, err := redis.Dial("tcp", REDIS)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-
-		// read and parse request body content
-		r.Body.Read(body)
-		data, err := url.ParseQuery(string(body))
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-
-		// create job
-		job := Job{
-			Class: CLASS,
-			Args: map[string]string {
-			"request": strings.TrimPrefix(data.Get("text"), data.Get("trigger_word")),
-			"user": data.Get("user_name"),
-			"channel": data.Get("channel_name"),
-			"timestamp": data.Get("timestamp"),
-			},
-		}
-		jjob, err := json.Marshal(job)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-		// push job to resque
-		queue := "resque:queue:" + QUEUE
-		rcon.Cmd("RPUSH", queue, string(jjob))
-		rcon.Close()
-
-		// response to slack
-		res := Response{
-			Text: "@" + data.Get("user_name") + ": " + strings.TrimPrefix(data.Get("text"), "slackops: "),
-		}
-		jres, err := json.Marshal(res)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-		w.Header().Set("Content-type", "application/json")
-		fmt.Fprintf(w, string(jres))
+	if r.Method != "POST" {
+		return
 	}
+
+	// read and parse request body content
+	body := make([]byte, r.ContentLength)
+	r.Body.Read(body)
+	data, err := url.ParseQuery(string(body))
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	// check token
+	if ! TOKENS[data.Get("token")] {
+		return
+	}
+
+	// make connection to redis
+	rcon, err := redis.Dial("tcp", REDIS)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	// create job
+	job := Job{
+		Class: CLASS,
+		Args: map[string]string {
+		"request": strings.TrimPrefix(data.Get("text"), data.Get("trigger_word")),
+		"user": data.Get("user_name"),
+		"channel": data.Get("channel_name"),
+		"timestamp": data.Get("timestamp"),
+		},
+	}
+	jjob, err := json.Marshal(job)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	// push job to resque
+	queue := "resque:queue:" + QUEUE
+	rcon.Cmd("RPUSH", queue, string(jjob))
+	rcon.Close()
+
+	// response to slack
+	res := Response{
+		Text: "@" + data.Get("user_name") + ": " + strings.TrimPrefix(data.Get("text"), "slackops: "),
+	}
+	jres, err := json.Marshal(res)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	w.Header().Set("Content-type", "application/json")
+	fmt.Fprintf(w, string(jres))
 }
