f375d65aa00b22f79908d75b9f525f072a6955ff
Viet Pham <viet.pham@audiencemedia.com>
2015-03-09 14:21:53 +0700

    filter with access list

--- slack-jobs.go
+++ slack-jobs.go
@@ -33,7 +33,7 @@ type AccessRule struct {
 	Deny_msg string
 }
 
-type AccessList []AccessRule
+type AccessList map[string]*AccessRule
 
 // some default global variable
 var (
@@ -45,7 +45,9 @@ var (
 	TOKENS map[string]bool = make(map[string]bool)
 	VERBOSE bool = false
 	FLAGS map[string]bool = make(map[string]bool)
-	ACCESS_LIST AccessList
+	ACCESS_LIST AccessList = make(AccessList)
+	DEFAULT_ALLOW_RESPONSE_TEXT = "please wait"
+	DEFAULT_DENY_RESPONSE_TEXT = "task denied"
 	err error
 )
 
@@ -138,7 +140,7 @@ func main() {
 					Allow_msg: allow_msg,
 					Deny_msg: deny_msg,
 				}
-				ACCESS_LIST = append(ACCESS_LIST, ar)
+				ACCESS_LIST[ar.Name] = &ar
 			}
 		}
 	}
@@ -220,34 +222,81 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// make connection to redis
-	rcon, err := redis.Dial("tcp", REDIS)
-	if err != nil {
-		fmt.Println("error:", err)
+	// get request and user name
+	request := strings.TrimPrefix(data.Get("text"), data.Get("trigger_word"))
+	request = strings.TrimLeft(request, " ")
+	request = strings.TrimRight(request, " ")
+	user := data.Get("user_name")
+
+	// filter with access list
+	var pass bool
+	var response_text string
+	if ACCESS_LIST[request] != nil {
+		// if user in deny list
+		if (!ACCESS_LIST[request].Policy && ACCESS_LIST[request].Users[user]) {
+			pass = false
+		}
+		// if user not in allow list
+		if (ACCESS_LIST[request].Policy && !ACCESS_LIST[request].Users[user]) {
+			pass = false
+		}
+		// if user is denied
+		if !pass {
+			if len(ACCESS_LIST[request].Deny_msg) > 0 {
+				response_text = ACCESS_LIST[request].Deny_msg
+			} else {
+				response_text = DEFAULT_DENY_RESPONSE_TEXT
+			}
+		}
+
+		// if user in allow list
+		if (ACCESS_LIST[request].Policy && ACCESS_LIST[request].Users[user]) {
+			pass = true
+		}
+		// if user not in deny list
+		if (!ACCESS_LIST[request].Policy && !ACCESS_LIST[request].Users[user]) {
+			pass = true
+		}
+		// if user is allowed
+		if pass {
+			if len(ACCESS_LIST[request].Allow_msg) > 0 {
+				response_text = ACCESS_LIST[request].Allow_msg
+			} else {
+				response_text = DEFAULT_ALLOW_RESPONSE_TEXT
+			}
+		}
 	}
 
-	// create job
-	job := Job{
-		Class: CLASS,
-		Args: map[string]string {
-		"request": strings.TrimPrefix(data.Get("text"), data.Get("trigger_word")),
-		"user": data.Get("user_name"),
-		"channel": data.Get("channel_name"),
-		"timestamp": data.Get("timestamp"),
-		},
+	if pass {
+		// make connection to redis
+		rcon, err := redis.Dial("tcp", REDIS)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		// create job
+		job := Job{
+			Class: CLASS,
+			Args: map[string]string {
+			"request": request,
+			"user": user,
+			"channel": data.Get("channel_name"),
+			"timestamp": data.Get("timestamp"),
+			},
+		}
+		jjob, err := json.Marshal(job)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+		// push job to resque
+		queue := "resque:queue:" + QUEUE
+		rcon.Cmd("RPUSH", queue, string(jjob))
+		rcon.Close()
 	}
-	jjob, err := json.Marshal(job)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	// push job to resque
-	queue := "resque:queue:" + QUEUE
-	rcon.Cmd("RPUSH", queue, string(jjob))
-	rcon.Close()
 
 	// response to slack
 	res := Response{
-		Text: "@" + data.Get("user_name") + ": " + strings.TrimPrefix(data.Get("text"), "slackops: "),
+		Text: "@" + user + ": " + response_text),
 	}
 	jres, err := json.Marshal(res)
 	if err != nil {
