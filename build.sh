f50c59238e4031de29363f2ae73bc0a3bb9c6c79
Viet Pham <phamhongviet52@gmail.com>
2016-05-30 07:19:51 +0700

    test before build

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ fi
 go get github.com/fzzy/radix/redis
 go get github.com/glacjay/goini
 
+./test.sh
+
 CGO_ENABLED=0 go build -ldflags "-s" -a -installsuffix cgo -o slack-jobs
 
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL" $DOCKER_REGISTRY
